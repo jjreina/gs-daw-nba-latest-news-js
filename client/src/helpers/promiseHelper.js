0e01700fda3be94d6a418b1abca6a39e105129d2
jjreina <jreinapascual@gmail.com>
2024-12-04 09:49:50 +0100

    Fix logical error in parserToClass condition to handle null and undefined values correctly

--- client/src/helpers/promiseHelper.js
+++ client/src/helpers/promiseHelper.js
@@ -2,7 +2,7 @@ import { News } from "../classes/News";
 
 export const parserToClass = (newsLiteralObject) => {
   return new Promise((resolve, reject) => {
-    if (newsLiteralObject !== null && newsLiteralObject !== undefined) {
+    if (newsLiteralObject !== null || newsLiteralObject !== undefined) {
       const newsClasses = newsLiteralObject.map((newLiteralObject) => {
         let newsClass = new News(
           newLiteralObject.title,
