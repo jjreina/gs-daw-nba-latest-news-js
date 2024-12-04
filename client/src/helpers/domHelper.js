442abc65e946fa1a1a29715ae6e8c3db65dfd532
jjreina <jreinapascual@gmail.com>
2024-12-04 09:01:41 +0100

    Refactor createNewsCards to use createNewsCard for cleaner code

--- client/src/helpers/domHelper.js
+++ client/src/helpers/domHelper.js
@@ -1,23 +1,28 @@
+// Creamos un elemento HTML con la estructura de una card de noticia
+const createNewsCard = (article) => {
+  const card = document.createElement("div");
+  card.classList.add("card");
+  const cardContent = document.createElement("div");
+  cardContent.classList.add("card-content");
+  card.appendChild(cardContent);
+  const cardTitle = document.createElement("h2");
+  cardTitle.classList.add("card-title");
+  cardTitle.textContent = article.source;
+  cardContent.appendChild(cardTitle);
+  const cardDescription = document.createElement("p");
+  cardDescription.classList.add("card-description");
+  cardDescription.textContent = article.title;
+  const cardURL = document.createElement("p");
+  cardURL.classList.add("card-description");
+  cardURL.textContent = article.url;
+  cardContent.append(cardDescription, cardURL);
+  return card;
+};
+
 // Devuelve un array de cards preparadas para ser renderizadas
 export const createNewsCards = (news) => {
   const cards = news.map((article) => {
-    const card = document.createElement("div");
-    card.classList.add("card");
-    const cardContent = document.createElement("div");
-    cardContent.classList.add("card-content");
-    card.appendChild(cardContent);
-    const cardTitle = document.createElement("h2");
-    cardTitle.classList.add("card-title");
-    cardTitle.textContent = article.source;
-    cardContent.appendChild(cardTitle);
-    const cardDescription = document.createElement("p");
-    cardDescription.classList.add("card-description");
-    cardDescription.textContent = article.title;
-    const cardURL = document.createElement("p");
-    cardURL.classList.add("card-description");
-    cardURL.textContent = article.url;
-    cardContent.append(cardDescription, cardURL);
-    return card;
+    return createNewsCard(article);
   });
   return cards;
 };
